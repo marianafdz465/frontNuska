2ef90cc7b5c6ac027fe2a9e08a2623fe568a52c4
Mariana Fernandez <marianafdz465@gmail.com>
2019-10-18 15:06:46 -0500

    trash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,14 @@ import { ArtesaniasService } from './servicios/artesanias.service';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
 import { ArtesanosComponent } from './components/artesanos/artesanos.component';
 import { ArtesanoComponent } from './components/artesano/artesano.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 import { ArtesanoTarjetaComponent } from './components/artesano-tarjeta/artesano-tarjeta.component';
 import { ArtesaniasComponent } from './components/artesanias/artesanias.component';
 import { ArtesaniaComponent } from './components/artesania/artesania.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
 
 
 
@@ -29,23 +30,24 @@ import { ArtesaniaComponent } from './components/artesania/artesania.component';
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    AboutComponent,
     ArtesanosComponent,
     ArtesanoComponent,
     BuscadorComponent,
     ArtesanoTarjetaComponent,
     ArtesaniasComponent,
-    ArtesaniaComponent
+    ArtesaniaComponent,
+    FooterComponent
 
   ],
   imports: [
     FormsModule,
-    BrowserModule,
+    // BrowserModule,
+    // HttpModule,
     APP_ROUTING
   ],
   providers: [
     ArtesanosService,
-    ArtesaniasService
+    ArtesaniasService,
   ],
   bootstrap: [AppComponent]
 })
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -12,7 +12,7 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
   }
 
-  buscarArtesania( termino: string ) {
+  buscarArtesano( termino: string ) {
     // console.log(termino);
     this.router.navigate( ['/buscar', termino] );
   }
